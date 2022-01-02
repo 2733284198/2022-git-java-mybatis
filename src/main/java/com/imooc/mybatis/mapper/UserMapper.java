251e485c034a7d254ce7d4728f0412ad6dd12144
qs-wanhongbin <manlan2010@163.com>
2022-01-03 00:01:55 +0800

    调用复用的 1、selectUserShortcutById

--- src/main/java/com/imooc/mybatis/mapper/UserMapper.java
+++ src/main/java/com/imooc/mybatis/mapper/UserMapper.java
@@ -1,6 +1,9 @@
 package com.imooc.mybatis.mapper;
 
 import com.imooc.mybatis.model.User;
+import com.imooc.mybatis.model.UserShortCut;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 public interface UserMapper {
@@ -12,6 +15,20 @@ public interface UserMapper {
 
 //    Integer selectUsernameById(Integer id);
     User selectUserByAgeAndScore(User user);
+
+    User selectUserByAgeAndScore2(User user);
+
+    @Results({
+            @Result(property = "id", column = "id", id = true),
+            @Result(property = "username", column = "username"),
+            @Result(property = "age", column = "age"),
+            @Result(property = "score", column = "score")
+    })
+    @Select("SELECT * FROM imooc_user WHERE id = #{id}")
+    User selectUserById(Integer id);
+
+    UserShortCut selectUserShortcutById(Integer id);
+
 }
 
 
